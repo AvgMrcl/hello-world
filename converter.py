6325c6c24934d61bda4bdc3e228afa0f87f4427c
AvgMrcl <64408002+AvgMrcl@users.noreply.github.com>
2020-09-23 19:23:54 +0200

    Created using Colaboratory

---
+++ converter.py
@@ -0,0 +1,49 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "converters.py",
+      "provenance": [],
+      "collapsed_sections": [],
+      "toc_visible": true,
+      "mount_file_id": "1OdUKaRW6sCXGiXVxfo-IaxNDgpOy3YNb",
+      "authorship_tag": "ABX9TyPh8dhvLTjvxPFB9c01V7NJ",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/AvgMrcl/hello-world/blob/master/converter.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "5wAzjs4iypaO",
+        "colab_type": "code",
+        "colab": {}
+      },
+      "source": [
+        "def lbs_to_kg(weight):\n",
+        "  return weight * 0.45\n",
+        "\n",
+        "\n",
+        "def kg_to_lbs(weight):\n",
+        "  return weight / 0.45"
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
